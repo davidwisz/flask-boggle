34264e49313b83b9fe6b9d2f047449a3299a2a81
Nicholai Hansen <nicholaihansen1993@gmail.com>
2020-01-24 16:39:07 -0800

    problems 2-3 and part of 4

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app.config['SECRET_KEY'] = "oh-so-secret"
 
 debug = DebugToolbarExtension(app)
 
-#create instance of Boggle class
+# create instance of Boggle class
 boggle_game = Boggle()
+
 
 @app.route("/")
 def begin():
@@ -16,12 +17,40 @@ def begin():
     boggle_board = boggle_game.make_board()
     # pass the board to Jinja template
     session["board"] = boggle_board
-    print (session["board"])
+    session['words'] = []
+    session['score'] = 0
+
     return render_template("index.html", board=boggle_board)
+
 
 @app.route("/word-check", methods=["POST"])
 def post_word():
-    data = request.form['word']
-    print("WORD DATA", jsonify(data))
-    return jsonify(data)
 
+    word = request.json['word']
+    word_exists = True if word in boggle_game.words else False
+    if word_exists:
+        word_on_board = True if boggle_game.check_valid_word(
+            session['board'], word) == "ok" else False
+
+        if word_on_board is True:
+            # Add word to words in session
+            words_list = session['words']
+            words_list.append(word)
+            session['words'] = words_list
+
+            # Add points to score in session
+            word_score = len(word)
+            session['score'] += word_score
+
+            # Update result dictionary
+            result = {'result': 'ok', 'word': word, 'score': word_score}
+        else:
+            # Update result dictionary
+            result = {'result': 'not-on-board', 'word': word}
+    else:
+        # Update result dictionary
+        result = {'result': 'not-a-word', 'word': word}
+
+    print("words in session", session['words'])
+    print("WORD DATA", word)
+    return jsonify(result)
--- static/boggle.js
+++ static/boggle.js
@@ -2,12 +2,34 @@ $(function(){
     //CREATE FORM SUBMIT HANDLER
     async function formSubmitHandler(searchWord) {
         let response = await axios.post('/word-check', {"word": searchWord});
-        console.log('here it is ' + response);
+        return response.data
     }
+    
     //CREATE FORM SUBMIT LISTENER
-    $('#word-entry-form').on('submit',  function(e){
+    $('#word-entry-form').on('submit', async function(e){
         e.preventDefault();
         let searchWord = $('#word_entry').val();
-        formSubmitHandler(searchWord);
+        let responseData = await formSubmitHandler(searchWord);
+        displayMessage(responseData);
     })
+
+    // DISPLAY USER MESSAGE
+    function displayMessage(responseData) {
+        $message = $('#message');
+        $message.removeClass('success failure');
+        $score = $('.score');
+
+        if (responseData.result === 'ok') {
+            $message.addClass('success');
+            $message.text(`Added: ${responseData.word}`);
+            $score.text(responseData.score)
+        } else if (responseData.result === 'not-a-word') {
+            $message.addClass('failure');
+            $message.text(`${responseData.word} is not a valid English word`);
+        } else {
+            $message.addClass('failure');
+            $message.text(`${responseData.word} is not a valid word on the board`)
+        }
+    }
+
 })
